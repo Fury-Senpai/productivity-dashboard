6423f6be23b3d8956d135b987a9ab69fbc2407cc
Fury-Senpai <harbola03nitish@gmail.com>
2026-02-19 22:47:20 +0530

    created toggleCard function

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-const allElem = document.querySelectorAll('.elem');
-const close = document.querySelectorAll('.back')
-const fullElem = document.querySelectorAll('.fullElem')
+function toggleCard(){
+    const allElem = document.querySelectorAll('.elem');
+    const close = document.querySelectorAll('.back')
+    const fullElem = document.querySelectorAll('.fullElem')
 
-allElem.forEach((elem , idx)=>{
-    elem.addEventListener('click' , ()=>{
-        fullElem[idx].style.display = 'block'    
-    });
-})
-
-close.forEach((closeElem , idx)=>{
-    closeElem.addEventListener('click' , ()=>{
-        fullElem[idx].style.display = 'none'    
+    allElem.forEach((elem , idx)=>{
+        elem.addEventListener('click' , ()=>{
+            fullElem[idx].style.display = 'block'    
+        });
     })
-})
+
+    close.forEach((closeElem , idx)=>{
+        closeElem.addEventListener('click' , ()=>{
+            fullElem[idx].style.display = 'none'    
+        })
+    })
+}
+
+toggleCard();
