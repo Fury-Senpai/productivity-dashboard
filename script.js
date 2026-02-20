f4623dd657144923005f8fabc3ae2bf36b99914a
Fury-Senpai <harbola03nitish@gmail.com>
2026-02-21 00:56:03 +0530

    completed todo page script.html

--- script.js
+++ script.js
@@ -17,3 +17,120 @@ function toggleCard(){
 }
 
 toggleCard();
+
+const form = document.querySelector('form');
+const ip = document.querySelectorAll('.ip');
+
+let tasksData = [];
+
+form.addEventListener('submit' , (e)=>{
+    e.preventDefault();
+    if(ip[0].value.trim('') === '' || ip[1].value.trim('') === '') return;
+    if(ip[0].value.length > 20) {
+        console.log("Title Limit Exceeded");
+        return;
+    }
+    console.dir(ip[2].checked);
+    newTask();
+})
+
+function newTask(){
+    const id = Date.now();
+    tasksData.push({
+        id : id,
+        title : ip[0].value,
+        details : ip[1].value,
+        important : ip[2].checked
+        
+    })
+
+    // Create main task div
+    const task = document.createElement("div");
+    task.classList.add("task");
+
+    task.dataset.id = id;
+
+    // Create heading
+    const heading = document.createElement("h5");
+    heading.classList.add('task-title')
+    
+
+    if(ip[2].checked === true) {
+        task.classList.add("imp-task")
+        const headingWrapper = document.createElement("div");
+        headingWrapper.classList.add('heading-wrapper');
+
+        const superTag = document.createElement("sup");
+        superTag.classList.add('h5-sup')
+        superTag.textContent = 'Imp'
+        heading.textContent = ` ${ip[0].value}`;
+        
+        headingWrapper.append(superTag)
+        
+        headingWrapper.append(heading)
+        task.appendChild(headingWrapper);
+    }
+    else{
+        task.appendChild(heading);
+        heading.textContent = ip[0].value;
+    }
+
+    // Create button wrapper
+    const btnWrapper = document.createElement("div");
+    btnWrapper.classList.add("btn-wrapper");
+
+    // Create Complete button
+    const completeBtn = document.createElement("button");
+    completeBtn.classList.add("completed-task-btn");
+    completeBtn.textContent = "Complete";
+
+    // Create Delete button
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("delete-task-btn");
+    deleteBtn.textContent = "Delete";
+
+    // Append buttons inside wrapper
+    btnWrapper.appendChild(completeBtn);
+    btnWrapper.appendChild(deleteBtn);
+
+    // Append heading + wrapper inside task
+    
+    task.appendChild(btnWrapper);
+
+    // Finally append task somewhere (example)
+    document.querySelector(".all-task").appendChild(task);
+    
+}
+
+
+const deleteBtns = document.querySelectorAll('.delete-task-btn');
+const allTask = document.querySelector('.all-task');
+
+allTask.addEventListener('click' , (e)=>{
+    if(e.target.classList.contains('delete-task-btn')) {
+        const task = e.target.closest('.task');
+        const id = Number(task.dataset.id);
+        tasksData = tasksData.filter(data => data.id !== id);
+        task.remove();
+        
+    }
+
+    
+    if(e.target.classList.contains('completed-task-btn')) {
+       
+        const task = e.target.closest('.task');
+        const taskTitle = task.querySelector('.task-title')
+        console.log(task);
+        if(e.target.classList.contains('task-completed')) {
+            e.target.classList.remove('task-completed');
+            taskTitle.classList.remove('task-strikethrough');
+            e.target.textContent = 'Complete'
+        }
+        else {
+            taskTitle.classList.add('task-strikethrough');
+            e.target.classList.add('task-completed');
+            e.target.textContent = 'Completed';
+
+        }
+    }
+})
